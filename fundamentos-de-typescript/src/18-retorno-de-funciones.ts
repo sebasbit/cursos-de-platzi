673a6edc842b4703a0fadc14d2d538e04b039f97
sebasbit <sebastian.torres0426@gmail.com>
2023-08-13 14:43:26 -0500

    Clase 18: retorno de funciones

---
+++ fundamentos-de-typescript/src/18-retorno-de-funciones.ts
@@ -0,0 +1,15 @@
+(() => {
+  const add = (...values: number[]): number => {
+    const total = values.reduce((a, b) => b += a);
+    return total;
+    // return total.toString() // Don't do this
+  };
+
+  const printTotal = (...values: number[]): void => {
+    const total = add(...values)
+    console.log(`Total: ${total}`);
+    // return total //  Don't do this
+  };
+
+  printTotal(1, 2, 3);
+})();
