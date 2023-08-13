d8773cb9a53b7d2915e70e8fdafad580986c1f4e
sebasbit <sebastian.torres0426@gmail.com>
2023-08-13 17:52:10 -0500

    Clase 23: Usando librerías que no soportan TypeScript

---
+++ fundamentos-de-typescript/src/23-librerias-que-no-soportan-typescript.ts
@@ -0,0 +1,27 @@
+import { groupBy } from 'lodash'
+
+const users = [
+  {
+    username: 'Aangg',
+    role: 'USER'
+  },
+  {
+    username: 'Katara',
+    role: 'USER'
+  },
+  {
+    username: 'Sokka',
+    role: 'ENTERTAINER'
+  },
+  {
+    username: 'Toph',
+    role: 'ADMIN'
+  },
+  {
+    username: 'Zuko',
+    role: 'ADMIN'
+  }
+];
+
+const group = groupBy(users, (u) => u.role);
+console.log(group);
