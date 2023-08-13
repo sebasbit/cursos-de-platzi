94ad1fb33687e290d99a7d3f3336ba3108f8bba0
sebasbit <sebastian.torres0426@gmail.com>
2023-08-13 15:12:07 -0500

    Clase 20: objetos como tipos

---
+++ fundamentos-de-typescript/src/20-objetos-como-tipos.ts
@@ -0,0 +1,19 @@
+(() => {
+  type Size = 'S' | 'M' | 'L' | 'XL';
+  type Product = {
+    title: string,
+    stock: number,
+    size?: Size,
+  };
+
+  const products: Product[] = [];
+
+  const saveProduct = (product: Product) => {
+    products.push(product);
+    // products.push(1); // Don't do this
+  };
+
+  saveProduct({title: 'T-Shirt', stock: 10, size: 'M'});
+  saveProduct({title: 'Jeans', stock: 5});
+  console.log(products);
+})();
