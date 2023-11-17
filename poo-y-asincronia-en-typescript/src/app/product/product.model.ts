959441f42da47d144790e5516ff8b2bfc9691eda
sebasbit <sebastian.torres0426@gmail.com>
2023-11-16 23:43:25 -0500

    Clase 16: tipando respuestas HTTP

---
+++ poo-y-asincronia-en-typescript/src/app/product/product.model.ts
@@ -0,0 +1,12 @@
+import { Category } from '../category/category.model';
+
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  images: string[];
+  creationAt: Date;
+  updatedAt: Date;
+  category: Category;
+}
