3839871d45d71e1d72e0b70e5a70b91a9f5fa31a
sebasbit <sebastian.torres0426@gmail.com>
2023-12-15 23:05:39 -0500

    Clase 3: mostrando elementos

--- angular-17-creacion-de-aplicaciones-web/src/app/pages/labs/labs.component.ts
+++ angular-17-creacion-de-aplicaciones-web/src/app/pages/labs/labs.component.ts
@@ -1,10 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css',
 })
-export class LabsComponent {}
+export class LabsComponent {
+  title = 'labs';
+  tasks = [
+    'Create a new project with "ng new project name"',
+    'Run the project with "ng serve"',
+    'Open your web browser on http://localhost:4200/',
+  ];
+  private word = 'very';
+
+  public getPrivateWord(): string {
+    return this.word;
+  }
+}
