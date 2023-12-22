424b3180d1d5b70c6bff73963341d032477c360d
sebasbit <sebastian.torres0426@gmail.com>
2023-12-21 22:50:39 -0500

    Clase 9: directivas de control

--- angular-17-creacion-de-aplicaciones-web/src/app/pages/home/home.component.ts
+++ angular-17-creacion-de-aplicaciones-web/src/app/pages/home/home.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  tasks = signal([
+    'Create a new project with "ng new project name"',
+    'Run the project with "ng serve"',
+    'Open your web browser on http://localhost:4200/',
+  ]);
+}
