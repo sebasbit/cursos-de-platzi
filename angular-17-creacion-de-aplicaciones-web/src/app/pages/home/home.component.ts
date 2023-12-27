e779fbe8ff79a13158b427aceb6e4e31a0438938
sebasbit <sebastian.torres0426@gmail.com>
2023-12-26 21:34:34 -0500

    Clase 11: ngFor para objetos

--- angular-17-creacion-de-aplicaciones-web/src/app/pages/home/home.component.ts
+++ angular-17-creacion-de-aplicaciones-web/src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +10,22 @@ import { Component, signal } from '@angular/core';
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  tasks = signal(['Clean my room', 'Do laundry', 'Work out']);
+  tasks = signal<Task[]>([
+    { id: 1, title: 'Clean my room', completed: true },
+    { id: 2, title: 'Do laundry', completed: false },
+    { id: 3, title: 'Work out', completed: false },
+  ]);
 
   addTask(event: Event): void {
     const input = event.target as HTMLInputElement;
-    this.tasks.update((tasks) => [...tasks, input.value]);
+    this.tasks.update((tasks) => [
+      ...tasks,
+      {
+        id: this.tasks().length + 1,
+        title: input.value,
+        completed: false,
+      },
+    ]);
     input.value = '';
   }
 
