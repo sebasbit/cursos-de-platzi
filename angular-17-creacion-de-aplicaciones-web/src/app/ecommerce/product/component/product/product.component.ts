9a12dfccd7a3918993c16b50d7c638e75c2c79e9
sebasbit <sebastian.torres0426@gmail.com>
2024-01-08 10:30:33 -0500

    Clase 31: uso de Inputs en Angular

--- angular-17-creacion-de-aplicaciones-web/src/app/ecommerce/product/component/product/product.component.ts
+++ angular-17-creacion-de-aplicaciones-web/src/app/ecommerce/product/component/product/product.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product',
   standalone: true,
   imports: [],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  styleUrl: './product.component.css',
 })
 export class ProductComponent {
-
+  @Input({ required: true }) image: string = '';
+  @Input({ required: true }) title: string = '';
+  @Input({ required: true }) price: number = 0;
 }
