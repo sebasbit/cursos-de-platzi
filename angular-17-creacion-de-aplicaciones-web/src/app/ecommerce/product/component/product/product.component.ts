89590bf6b4d248fbf5120763b21bc5165f3056ea
sebasbit <sebastian.torres0426@gmail.com>
2024-01-13 13:31:22 -0500

    Clase 41: creando componente de productos

--- angular-17-creacion-de-aplicaciones-web/src/app/ecommerce/product/component/product/product.component.ts
+++ angular-17-creacion-de-aplicaciones-web/src/app/ecommerce/product/component/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../../shared/model/product.model';
 
 @Component({
   selector: 'app-product',
@@ -8,13 +9,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './product.component.css',
 })
 export class ProductComponent {
-  @Input({ required: true }) image: string = '';
-  @Input({ required: true }) title: string = '';
-  @Input({ required: true }) price: number = 0;
+  @Input({ required: true }) product!: Product;
 
   @Output() addToCart = new EventEmitter();
 
   emitAddToCart(): void {
-    this.addToCart.emit(`Add product: ${this.title}`);
+    this.addToCart.emit(`Add product: ${this.product.title}`);
   }
 }
--- angular-17-creacion-de-aplicaciones-web/src/app/ecommerce/product/page/list/list.component.ts
+++ angular-17-creacion-de-aplicaciones-web/src/app/ecommerce/product/page/list/list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../component/product/product.component';
+import { Product } from '../../../shared/model/product.model';
 
 @Component({
   selector: 'app-list',
@@ -9,6 +10,27 @@ import { ProductComponent } from '../../component/product/product.component';
   styleUrl: './list.component.css',
 })
 export class ListComponent {
+  products = signal<Product[]>([]);
+
+  constructor() {
+    this.products.set([
+      {
+        id: 1,
+        title: 'Product example #1',
+        price: 1000,
+        image: 'https://cataas.com/cat?width=640&height=640&r=1',
+        createdAt: new Date(),
+      },
+      {
+        id: 2,
+        title: 'Product example #2',
+        price: 2000,
+        image: 'https://cataas.com/cat?width=640&height=640&r=2',
+        createdAt: new Date(),
+      },
+    ]);
+  }
+
   handleAddToCart(event: string): void {
     alert(event);
   }
