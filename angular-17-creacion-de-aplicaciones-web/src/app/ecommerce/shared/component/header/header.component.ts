9c2c123bf2d9bcdea8efff8ea934525f789f5160
sebasbit <sebastian.torres0426@gmail.com>
2024-01-14 13:05:16 -0500

    Clase 43: creando el carrito de compras

--- angular-17-creacion-de-aplicaciones-web/src/app/ecommerce/shared/component/header/header.component.ts
+++ angular-17-creacion-de-aplicaciones-web/src/app/ecommerce/shared/component/header/header.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styles: ``
+  styles: ``,
 })
 export class HeaderComponent {
+  hideSideMenu = signal(true);
 
+  toggleSideMenu(): void {
+    this.hideSideMenu.update((value) => !value);
+  }
 }
