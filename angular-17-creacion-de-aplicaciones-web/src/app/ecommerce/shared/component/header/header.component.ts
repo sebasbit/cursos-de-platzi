58c8e2ac1fc94a4b5da327419d66a97682f32218
sebasbit <sebastian.torres0426@gmail.com>
2024-01-16 22:36:09 -0500

    Clase 45: calculando el total con ngOnChanges

--- angular-17-creacion-de-aplicaciones-web/src/app/ecommerce/shared/component/header/header.component.ts
+++ angular-17-creacion-de-aplicaciones-web/src/app/ecommerce/shared/component/header/header.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, signal } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  signal,
+} from '@angular/core';
 import { Product } from '../../model/product.model';
 
 @Component({
@@ -8,12 +14,23 @@ import { Product } from '../../model/product.model';
   templateUrl: './header.component.html',
   styles: ``,
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnChanges {
   @Input({ required: true }) cart: Product[] = [];
 
   hideSideMenu = signal(true);
+  totalPrice = signal(0);
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['cart'] !== undefined) {
+      this.totalPrice.set(this.calcTotalPrice());
+    }
+  }
 
   toggleSideMenu(): void {
     this.hideSideMenu.update((value) => !value);
+  }
+
+  private calcTotalPrice(): number {
+    return this.cart.reduce((acum, product) => acum + product.price, 0);
   }
 }
