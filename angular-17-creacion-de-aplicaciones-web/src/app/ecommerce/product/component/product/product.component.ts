e8e75c93d983bdfb5786cba13a67998ed8022419
sebasbit <sebastian.torres0426@gmail.com>
2024-01-19 23:58:52 -0500

    Clase 51: pipes en Angular

--- angular-17-creacion-de-aplicaciones-web/src/app/ecommerce/product/component/product/product.component.ts
+++ angular-17-creacion-de-aplicaciones-web/src/app/ecommerce/product/component/product/product.component.ts
@@ -1,10 +1,11 @@
+import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '@shared/model/product.model';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [UpperCasePipe, CurrencyPipe, DatePipe],
   templateUrl: './product.component.html',
   styles: ``,
 })
