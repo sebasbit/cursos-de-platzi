04221fef97850c645799ef204bceea02f6c35dbc
sebasbit <sebastian.torres0426@gmail.com>
2024-01-22 21:26:40 -0500

    Clase 59: uso del RouterLinkActive

--- angular-17-creacion-de-aplicaciones-web/src/app/ecommerce/shared/component/header/header.component.ts
+++ angular-17-creacion-de-aplicaciones-web/src/app/ecommerce/shared/component/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
 import { CartService } from '../../service/cart.service';
-import { RouterLinkWithHref } from '@angular/router';
+import { RouterLinkActive, RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLinkWithHref],
+  imports: [RouterLinkWithHref, RouterLinkActive],
   templateUrl: './header.component.html',
   styles: ``,
 })
