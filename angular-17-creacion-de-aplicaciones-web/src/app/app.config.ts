882aa173aa2e7c7afe4b729c089e9c5b41e7061d
sebasbit <sebastian.torres0426@gmail.com>
2024-01-26 22:19:09 -0500

    Clase 68: prefetching

--- angular-17-creacion-de-aplicaciones-web/src/app/app.config.ts
+++ angular-17-creacion-de-aplicaciones-web/src/app/app.config.ts
@@ -1,11 +1,20 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import {
+  PreloadAllModules,
+  provideRouter,
+  withComponentInputBinding,
+  withPreloading,
+} from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withComponentInputBinding()),
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withPreloading(PreloadAllModules)
+    ),
     provideHttpClient(),
   ],
 };
