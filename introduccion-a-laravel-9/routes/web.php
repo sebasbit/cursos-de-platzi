13dcb782e4e3db87f37049a633cfa3f4000208ac
sebasbit <sebastian.torres0426@gmail.com>
2024-03-09 12:53:19 -0500

    Clase 4: Routes

--- introduccion-a-laravel-9/routes/web.php
+++ introduccion-a-laravel-9/routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/lucky-number', fn () => '<h1>Your lucky number is: <u>' . rand(1000, 9999) . '</u>!</h1>');
+
+Route::get('/greet/{name}', fn (string $name) => "<h1>Hi <u>{$name}</u>!</h1>");
+
+Route::get('/search', fn (Request $request) => "<h1>Looking for <u>{$request->get('q', 'anything')}</u>...</h1>");
