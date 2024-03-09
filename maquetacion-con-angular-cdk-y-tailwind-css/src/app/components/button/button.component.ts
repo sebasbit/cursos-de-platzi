0b15a56affdebe8b19b74b31791a7de09b1f0c31
sebasbit <sebastian.torres0426@gmail.com>
2024-03-08 23:00:23 -0500

    Clase 6: colores y propiedades dinámicas

--- maquetacion-con-angular-cdk-y-tailwind-css/src/app/components/button/button.component.ts
+++ maquetacion-con-angular-cdk-y-tailwind-css/src/app/components/button/button.component.ts
@@ -1,17 +1,34 @@
+import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
+
+type BtnType = 'button' | 'reset' | 'submit';
+type Color = 'primary' | 'success' | 'default';
 
 @Component({
   selector: 'app-button',
   standalone: true,
+  imports: [NgClass],
   template: `
     <button
       [type]="btnType"
-      class="text-white w-full font-medium rounded text-sm px-5 py-2 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300"
+      class="text-white w-full font-medium rounded text-sm px-5 py-2 focus:ring-4"
+      [ngClass]="colorClasses"
     >
       <ng-content></ng-content>
     </button>
   `,
 })
 export class ButtonComponent {
-  @Input() btnType: 'button' | 'reset' | 'submit' = 'button';
+  @Input() btnType: BtnType = 'button';
+  @Input() color: Color = 'default';
+
+  get colorClasses(): string {
+    const classList: { [color in Color]: string } = {
+      primary: 'bg-primary-700 hover:bg-primary-800 focus:ring-primary-300',
+      success: 'bg-success-700 hover:bg-success-800 focus:ring-success-300',
+      default: 'bg-gray-700 hover:bg-gray-800 focus:ring-gray-300',
+    };
+
+    return classList[this.color] ?? classList['default'];
+  }
 }
