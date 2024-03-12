e7148d239acbe8e0f0daef879e6fe4659c9e2602
sebasbit <sebastian.torres0426@gmail.com>
2024-03-12 00:41:28 -0500

    Clase 10: creando acordeones

--- maquetacion-con-angular-cdk-y-tailwind-css/src/app/pages/boards/boards.component.ts
+++ maquetacion-con-angular-cdk-y-tailwind-css/src/app/pages/boards/boards.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faTrello } from '@fortawesome/free-brands-svg-icons';
 import {
+  faAngleDown,
+  faAngleUp,
+  faBorderAll,
   faBox,
   faClock,
+  faGear,
+  faHeart,
+  faUsers,
   faWaveSquare,
 } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [NavbarComponent, FaIconComponent],
+  imports: [NavbarComponent, FaIconComponent, CdkAccordionModule],
   templateUrl: './boards.component.html',
 })
 export class BoardsComponent {
@@ -19,4 +26,10 @@ export class BoardsComponent {
   faBox = faBox;
   faWaveSquare = faWaveSquare;
   faClock = faClock;
+  faAngleUp = faAngleUp;
+  faAngleDown = faAngleDown;
+  faHeart = faHeart;
+  faBorderAll = faBorderAll;
+  faUsers = faUsers;
+  faGear = faGear;
 }
