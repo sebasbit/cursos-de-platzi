f98a18a0e655326956d72868de0fcb6d15a2a74f
sebasbit <sebastian.torres0426@gmail.com>
2024-03-15 00:11:24 -0500

    Clase 13: drag and drop con transferencia entre columnas

--- maquetacion-con-angular-cdk-y-tailwind-css/src/app/models/task.model.ts
+++ maquetacion-con-angular-cdk-y-tailwind-css/src/app/models/task.model.ts
@@ -1,4 +1,4 @@
-export interface ToDo {
+export interface Task {
   id: string;
   title: string;
 }
--- maquetacion-con-angular-cdk-y-tailwind-css/src/app/pages/board/board.component.ts
+++ maquetacion-con-angular-cdk-y-tailwind-css/src/app/pages/board/board.component.ts
@@ -5,9 +5,10 @@ import {
   CdkDropList,
   CdkDropListGroup,
   moveItemInArray,
+  transferArrayItem,
 } from '@angular/cdk/drag-drop';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { ToDo } from '../../models/todo.model';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-board',
@@ -17,22 +18,43 @@ import { ToDo } from '../../models/todo.model';
   styleUrl: './board.component.scss',
 })
 export class BoardComponent {
-  todos: ToDo[] = [
+  todo: Task[] = [
     {
-      id: 'ea247b32-7c41-40b0-b7eb-5a20bc51c28a',
-      title: 'HU01: Create new Angular app',
+      id: '916ba3f7-217d-4038-8c9d-6f3c99575157',
+      title: 'HU03: Create components for the UI',
     },
     {
-      id: 'ea247b32-7c41-40b0-b7eb-5a20bc51c28a',
+      id: '5be18df5-f703-4659-b589-157ed4a7d065',
+      title: 'HU04: Create the Log in page',
+    },
+  ];
+  doing: Task[] = [
+    {
+      id: 'cd5a8af6-39d8-4fd0-a7de-3916c1afb7cf',
       title: 'HU02: Add the Angular CDK',
     },
+  ];
+  done: Task[] = [
     {
       id: 'ea247b32-7c41-40b0-b7eb-5a20bc51c28a',
-      title: 'HU03: Create components for the UI',
+      title: 'HU02: Create new Angular app',
     },
   ];
 
-  drop(event: CdkDragDrop<string[]>): void {
-    moveItemInArray(this.todos, event.previousIndex, event.currentIndex);
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
   }
 }
