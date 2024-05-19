deaf276380686ba1bb870a2b2243165fd8e2bd51
sebasbit <sebastian.torres0426@gmail.com>
2024-05-18 22:20:53 -0500

    Clase 16: listado de publicaciones

--- introduccion-a-laravel-9/app/Http/Controllers/PostController.php
+++ introduccion-a-laravel-9/app/Http/Controllers/PostController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+
 class PostController extends Controller
 {
     function index() {
-        return view('post.index');
+        return view('post.index', [
+            'posts' => Post::latest()->paginate()
+        ]);
     }
 }
--- introduccion-a-laravel-9/resources/views/post/index.blade.php
+++ introduccion-a-laravel-9/resources/views/post/index.blade.php
@@ -9,7 +9,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __('List of publications') }}
+                    @forelse ($posts as $post)
+                    <div class="max-w-sm p-6 mb-4 bg-white border border-gray-200 rounded-lg shadow">
+                        <a href="#">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ $post->title }}</h5>
+                        </a>
+                        <p class="mb-4 font-normal text-gray-700">{{ Str::limit($post->content, 128) }}</p>
+                        <x-primary-button>Read more</x-primary-button>
+                    </div>
+                    @empty
+                    <p class="font-normal text-gray-700">{{ __('There are no publications') }}</p>
+                    @endforelse
+
+                    {{ $posts->links() }}
                 </div>
             </div>
         </div>
