67578d4a6b7467a33b712f92f89733893855fe2c
sebasbit <sebastian.torres0426@gmail.com>
2024-05-19 10:32:33 -0500

    Clase 18 y 19: crear y editar y controles de formulario

--- introduccion-a-laravel-9/app/Http/Controllers/PostController.php
+++ introduccion-a-laravel-9/app/Http/Controllers/PostController.php
@@ -10,6 +10,16 @@ class PostController extends Controller
         return view('post.index')
             ->with('posts', Post::latest()->paginate());
     }
+
+    public function create() {
+        return view('post.create');
+    }
+
+    public function edit(Post $post) {
+        return view('post.edit')
+            ->with('post', $post);
+    }
+
     public function destroy(Post $post) {
         $post->delete();
         return redirect()->route('post.index');
--- introduccion-a-laravel-9/resources/views/post/index.blade.php
+++ introduccion-a-laravel-9/resources/views/post/index.blade.php
@@ -1,8 +1,11 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Posts') }}
-        </h2>
+        <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Posts') }}
+            </h2>
+            <x-link-button href="{{ route('post.create') }}">{{ __('Create') }}</x-link-button>
+        </div>
     </x-slot>
 
     <div class="py-12">
@@ -11,12 +14,15 @@
                 <div class="p-6 text-gray-900">
                     @forelse ($posts as $post)
                     <div class="max-w-sm p-6 mb-4 bg-white border border-gray-200 rounded-lg shadow">
-                        <a href="#">
+                        <div class="flex justify-between">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ $post->title }}</h5>
-                        </a>
+                            <div>
+                                <x-link-button href="{{ route('post.edit', $post) }}">{{ __('Edit') }}</x-link-button>
+                                <x-delete-button action="{{ route('post.destroy', $post) }}">{{ __('Delete') }}</x-delete-button>
+                            </div>
+                        </div>
                         <p class="mb-4 font-normal text-gray-700">{{ Str::limit($post->content, 128) }}</p>
                         <x-primary-button>{{ __('Read more') }}</x-primary-button>
-                        <x-delete-button action="{{ route('post.destroy', $post) }}">{{ __('Delete') }}</x-delete-button>
                     </div>
                     @empty
                     <p class="font-normal text-gray-700">{{ __('There are no publications') }}</p>
